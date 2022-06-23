f5c24e9b61842f8f4538e1016dba3f1b4a9b177a
akale21 <78691845+akale21@users.noreply.github.com>
2022-06-23 18:59:56 +0530

    Add files via upload

---
+++ Count.js
@@ -0,0 +1,9 @@
+var adder=function(a,b){
+    return `The sum of 2 number is $(a+b)`;
+};
+
+var pi = 3.142;
+
+module.exports.counter=counter;
+module.exports.adder=adder;
+module.exports.pi=pi;
---
+++ Function.js
@@ -0,0 +1,11 @@
+function callFunction(fun){
+fun();
+}
+
+
+//function expression
+var saybye=function(){
+    console.log('hi');
+};
+
+callFunction(sayhi);
